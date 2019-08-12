fc9000b0722b376be3492da4137bd30359edaee3
Ryan Coughlin <ryan.coughs@gmail.com>
2019-08-11 21:54:38 -0400

    refactor v5

--- src/index.js
+++ src/index.js
@@ -1,64 +1,21 @@
 import * as PIXI from 'pixi.js';
 import Question from './question.js'
 import Answer from './answer.js'
-
-function showResume() {
-    var pdfjsLib = require('pdfjs-dist');
-    var pdfPath = 'assets/docs/ryan_coughlin_resume.pdf';
-    // $("a").attr("href", pdfPath); //set download path
-    
-    var loadingTask = pdfjsLib.getDocument(pdfPath);
-
-    var loadingTask = pdfjsLib.getDocument(pdfPath);
-    loadingTask.promise.then(function (pdfDocument) {
-        // Request a first page
-        return pdfDocument.getPage(1).then(function (pdfPage) {
-            // Display page on the existing canvas with 100% scale.
-
-            var canvas = document.getElementById('pdfjs');
-            canvas.style.display = "inherit"
-
-            app.view.style.display = "none";
-
-            var viewport = pdfPage.getViewport(3.0);
-
-            canvas.width = viewport.width;
-            canvas.height = viewport.height;
-            var ctx = canvas.getContext('2d');
-            var renderTask = pdfPage.render({
-                canvasContext: ctx,
-                viewport: viewport
-            });
-            return renderTask.promise;
-        });
-    }).catch(function (reason) {
-        console.error('Error: ' + reason);
-    });
-}
-
-
-
-function nextQuestion() {
-  if (currentQuestion) currentQuestion.remove();
-  currentQuestion = questions.shift();
-  currentQuestion.attach(app);
-}
-
-
+import { showResume, showChinese } from './util.js';
 
 let type = "WebGL"
-if(!PIXI.utils.isWebGLSupported()){
+if (!PIXI.utils.isWebGLSupported()) {
   type = "canvas"
 }
 
 //Create a Pixi Application
-let app = new PIXI.Application({ 
-    width: window.innerWidth, 
-    height: window.innerHeight,                       
-    antialias: true, 
-    transparent: false, 
-    resolution: 1
-  }
+let app = new PIXI.Application({
+  width: window.innerWidth,
+  height: window.innerHeight,
+  antialias: true,
+  transparent: false,
+  resolution: 1
+}
 );
 
 //Add the canvas that Pixi automatically created for you to the HTML document
@@ -66,15 +23,15 @@ document.body.appendChild(app.view);
 
 // load sprite sheet image + data file, call setup() if completed
 app.loader
-    .add("assets/img/title.png")
-    .add("assets/img/world4.png")
-    .add("assets/img/snake.json")
-    .add("assets/img/knight.json")
-    .add("assets/img/shrek2.png")
-    .add("assets/img/speech2.png")
-    .load(setup);
+  .add("assets/img/title.png")
+  .add("assets/img/world4.png")
+  .add("assets/img/snake.json")
+  .add("assets/img/knight.json")
+  .add("assets/img/shrek2.png")
+  .add("assets/img/speech2.png")
+  .load(setup);
 
-let python, knights, state, sheet, background, shrek, speech, title, currentQuestion;
+let python, knights, state, sheet, background, shrek, speech, title, currentQuestion, action;
 
 let names = [
   new Answer("Employer", () => console.log("Employer"), nextQuestion),
@@ -84,17 +41,17 @@ let names = [
 ];
 
 let quests = [
-  new Answer("Explore My Résumé", showResume, nextQuestion),
-  new Answer("Venture to The Orient", () => console.log("Friend"), nextQuestion),
+  new Answer("Explore My Résumé", () => action = showResume, nextQuestion),
+  new Answer("Venture to The Orient", () => action = showChinese, nextQuestion),
   new Answer("Seek the Holy Grail", () => console.log("Family"), nextQuestion),
   new Answer("Fight the Ogre", () => console.log("Foe"), nextQuestion)
 ];
 
 let colors = [
-  new Answer("Yellow", () => console.log("Yellow"), () => console.log("end")),
-  new Answer("Green", () => console.log("Green"), () => console.log("end")),
-  new Answer("Blue", () => console.log("Blue"), () => console.log("end")),
-  new Answer("Blue... No, Yellow!", () => console.log("undecided"), () => console.log("end"))
+  new Answer("Yellow", () => console.log("Yellow"), doAction),
+  new Answer("Green", () => console.log("Green"), doAction),
+  new Answer("Blue", () => console.log("Blue"), doAction),
+  new Answer("Blue... No, Yellow!", () => console.log("undecided"), () => console.log("TBA"))
 ];
 
 let questions = [
@@ -103,161 +60,132 @@ let questions = [
   new Question("What is your favorite color?", colors)
 ]
 
-function setup() {
-
-    background = new PIXI.Sprite(app.loader.resources["assets/img/world4.png"].texture);
-    background.width = window.innerWidth*2.5;
-    background.height = window.innerHeight;
-    app.stage.addChild(background);
-
-    title = new PIXI.Sprite(app.loader.resources["assets/img/title.png"].texture);
-    title.width = 0;
-    title.height = 0;
-    title.position.set(window.innerWidth/2, window.innerHeight/2);
-    title.anchor.x=.5
-    title.anchor.y=.5
-    app.stage.addChild(title);
-
-    shrek = new PIXI.Sprite(app.loader.resources["assets/img/shrek2.png"].texture);
-    shrek.width = shrek.width/2.5;
-    shrek.height = shrek.height/2.5;
-    shrek.position.set(2400, window.innerHeight - 420);
-    app.stage.addChild(shrek);
-
-    speech = new PIXI.Sprite(app.loader.resources["assets/img/speech2.png"].texture);
-    speech.width = speech.width/1.5;
-    speech.height = speech.height/2.5;
-    speech.position.set(1400, window.innerHeight - 800);
-    speech.visible = false;
-    app.stage.addChild(speech);
-
-    nextQuestion();
-
-    // the sprite sheet we've just loaded:
-    sheet = app.loader.resources["assets/img/snake.json"].spritesheet;
-
-    // create an animated sprite
-    python = new PIXI.AnimatedSprite(sheet.animations["snake_idle"]);
-
-    // configure + start animation:
-    python.position.set(600, window.innerHeight - 190); // almost bottom-left corner of the canvas
-    python.animationSpeed = 0.4;
-    python.anchor.x=.5
-    python.anchor.y=.5
-    python.scale.x=1
-    python.vx=0
-    python.vy=0
-    python.play();
-
-    python.width=1000;
-    python.height=1000;
-
-    // add it to the stage and render!
-    app.stage.addChild(python);
-
-    addKnights();
-
-    let left = keyboard("ArrowLeft"),
-    up = keyboard("ArrowUp"),
-    right = keyboard("ArrowRight"),
-    down = keyboard("ArrowDown");
-
-    // up.press = () => {
-      
-    // }
-
-    //Left arrow key `press` method
-    left.press = () => {
-    //Change the python's velocity when the key is pressed
-    python.vx = -1;
-    python.vy = 0;
-    python.scale.x=-1
-
-    python.width=1000;
-    python.height=1000;
-    python.textures = sheet.animations["snake_run"];
-    python.play()
-    playKnights();
-    };
-
-    //Left arrow key `release` method
-    left.release = () => {
-    //If the left arrow has been released, and the right arrow isn't down,
-    //and the python isn't moving vertically:
-    //Stop the python
-    if (!right.isDown && python.vy === 0) {
-        python.vx = 0;
-        python.textures = sheet.animations["snake_idle"];
-        python.width=1000;
-        python.height=1000;
-        python.play()
-        stopKnights();
-    }
-    };
-
-    //Up
-    // up.press = () => {
-    // python.vy = -3;
-    // python.vx = 0;
-    // };
-    // up.release = () => {
-    // if (!down.isDown && python.vx === 0) {
-    //     python.vy = 0;
-    // }
-    // };
-
-    //Right
-    right.press = () => {
-    python.vx = 1;
-    python.vy = 0;
-    python.scale.x=1
-    python.textures = sheet.animations["snake_run"];
-    python.width=1000;
-    python.height=1000;
-    python.play()
-    playKnights();
-    };
-    right.release = () => {
-    if (!left.isDown && python.vy === 0) {
-        python.vx = 0;
-        python.textures = sheet.animations["snake_idle"];
-        python.width=1000;
-        python.height=1000;
-        python.play()
-        stopKnights();
-    }
-    };
-
-    //Down
-    // down.press = () => {
-    // python.vy = 5;
-    // python.vx = 0;
-    // };
-    // down.release = () => {
-    // if (!up.isDown && python.vx === 0) {
-    //     python.vy = 0;
-    // }
-    // };
-
-
-
-
-
-    //Set the game state
-    state = play;
-    
-    //Start the game loop 
-    app.ticker.add(delta => gameLoop(delta));
+function nextQuestion() {
+  if (currentQuestion) currentQuestion.remove();
+  currentQuestion = questions.shift();
+  currentQuestion.attach(app);
 }
 
-function gameLoop(delta){
-    //Update the current game state:
+function doAction() {
+  action(app);
+}
 
-    state(delta);
+function setup() {
+
+  // load sprites
+  background = new PIXI.Sprite(app.loader.resources["assets/img/world4.png"].texture);
+  background.width = window.innerWidth * 2.5;
+  background.height = window.innerHeight;
+  app.stage.addChild(background);
+
+  title = new PIXI.Sprite(app.loader.resources["assets/img/title.png"].texture);
+  title.width = 0;
+  title.height = 0;
+  title.position.set(window.innerWidth / 2, window.innerHeight / 2);
+  title.anchor.x = .5
+  title.anchor.y = .5
+  app.stage.addChild(title);
+
+  shrek = new PIXI.Sprite(app.loader.resources["assets/img/shrek2.png"].texture);
+  shrek.width = shrek.width / 2.5;
+  shrek.height = shrek.height / 2.5;
+  shrek.position.set(2400, window.innerHeight - 420);
+  app.stage.addChild(shrek);
+
+  speech = new PIXI.Sprite(app.loader.resources["assets/img/speech2.png"].texture);
+  speech.width = speech.width / 1.5;
+  speech.height = speech.height / 2.5;
+  speech.position.set(1400, window.innerHeight - 800);
+  speech.visible = false;
+  app.stage.addChild(speech);
+
+  sheet = app.loader.resources["assets/img/snake.json"].spritesheet;
+  python = new PIXI.AnimatedSprite(sheet.animations["snake_idle"]);
+  python.position.set(600, window.innerHeight - 190); // almost bottom-left corner of the canvas
+  python.animationSpeed = 0.4;
+  python.anchor.x = .5
+  python.anchor.y = .5
+  python.scale.x = 1
+  python.vx = 0
+  python.vy = 0
+  python.play();
+  python.width = 1000;
+  python.height = 1000;
+  app.stage.addChild(python);
+
+  addKnights();
+
+
+  // load first question
+  nextQuestion();
+
+
+  let left = keyboard("ArrowLeft"),
+    right = keyboard("ArrowRight"),
+    up = keyboard("ArrowUp"),
+    down = keyboard("ArrowDown");
+
+  // Left
+  left.press = () => {
+    python.vx = -1;
+    python.vy = 0;
+    python.scale.x = -1
+
+    python.width = 1000;
+    python.height = 1000;
+    python.textures = sheet.animations["snake_run"];
+    python.play()
+    playKnights();
+  };
+
+  left.release = () => {
+    if (!right.isDown && python.vy === 0) {
+      python.vx = 0;
+      python.textures = sheet.animations["snake_idle"];
+      python.width = 1000;
+      python.height = 1000;
+      python.play()
+      stopKnights();
+    }
+  };
+
+  // Right
+  right.press = () => {
+    python.vx = 1;
+    python.vy = 0;
+    python.scale.x = 1
+    python.textures = sheet.animations["snake_run"];
+    python.width = 1000;
+    python.height = 1000;
+    python.play()
+    playKnights();
+  };
+
+  right.release = () => {
+    if (!left.isDown && python.vy === 0) {
+      python.vx = 0;
+      python.textures = sheet.animations["snake_idle"];
+      python.width = 1000;
+      python.height = 1000;
+      python.play()
+      stopKnights();
+    }
+  };
+
+  //Set the game state
+  state = play;
+
+  //Start the game loop 
+  app.ticker.add(delta => gameLoop(delta));
+}
+
+function gameLoop(delta) {
+  //Update the current game state:
+  state(delta);
 }
 
 function play(delta) {
-  const ratio = 16/9;
+  const ratio = 16 / 9;
 
   if (title.height < window.innerHeight) {
     title.width += 6 * ratio;
@@ -284,94 +212,94 @@ function play(delta) {
 }
 
 function keyboard(value) {
-    let key = {};
-    key.value = value;
-    key.isDown = false;
-    key.isUp = true;
-    key.press = undefined;
-    key.release = undefined;
-    //The `downHandler`
-    key.downHandler = event => {
-      if (event.key === key.value) {
-        if (key.isUp && key.press) key.press();
-        key.isDown = true;
-        key.isUp = false;
-        event.preventDefault();
-      }
-    };
-  
-    //The `upHandler`
-    key.upHandler = event => {
-      if (event.key === key.value) {
-        if (key.isDown && key.release) key.release();
-        key.isDown = false;
-        key.isUp = true;
-        event.preventDefault();
-      }
-    };
-  
-    //Attach event listeners
-    const downListener = key.downHandler.bind(key);
-    const upListener = key.upHandler.bind(key);
-    
-    window.addEventListener(
-      "keydown", downListener, false
-    );
-    window.addEventListener(
-      "keyup", upListener, false
-    );
-    
-    // Detach event listeners
-    key.unsubscribe = () => {
-      window.removeEventListener("keydown", downListener);
-      window.removeEventListener("keyup", upListener);
-    };
-    
-    return key;
+  let key = {};
+  key.value = value;
+  key.isDown = false;
+  key.isUp = true;
+  key.press = undefined;
+  key.release = undefined;
+  //The `downHandler`
+  key.downHandler = event => {
+    if (event.key === key.value) {
+      if (key.isUp && key.press) key.press();
+      key.isDown = true;
+      key.isUp = false;
+      event.preventDefault();
+    }
+  };
+
+  //The `upHandler`
+  key.upHandler = event => {
+    if (event.key === key.value) {
+      if (key.isDown && key.release) key.release();
+      key.isDown = false;
+      key.isUp = true;
+      event.preventDefault();
+    }
+  };
+
+  //Attach event listeners
+  const downListener = key.downHandler.bind(key);
+  const upListener = key.upHandler.bind(key);
+
+  window.addEventListener(
+    "keydown", downListener, false
+  );
+  window.addEventListener(
+    "keyup", upListener, false
+  );
+
+  // Detach event listeners
+  key.unsubscribe = () => {
+    window.removeEventListener("keydown", downListener);
+    window.removeEventListener("keyup", upListener);
+  };
+
+  return key;
 }
 
 function addKnights() {
-    let sheet2 = app.loader.resources["assets/img/knight.json"].spritesheet;
-    knights = []
+  let sheet2 = app.loader.resources["assets/img/knight.json"].spritesheet;
+  knights = []
 
-    for (let i = 0; i < 3; i++) {
-        // create an animated sprite
-        let knight = new PIXI.AnimatedSprite(sheet2.animations["knight"]);
+  for (let i = 0; i < 3; i++) {
+    // create an animated sprite
+    let knight = new PIXI.AnimatedSprite(sheet2.animations["knight"]);
 
-        // configure + start animation:
-        knight.position.set((i+1) * 100, window.innerHeight - 180); // almost bottom-left corner of the canvas
-        knight.animationSpeed = 0.25;
-        knight.anchor.x=.5
-        knight.anchor.y=.5
-        knight.scale.x=1
-        knight.vx=0
-        knight.vy=0
-        knight.gotoAndStop(1);
+    // configure + start animation:
+    knight.position.set((i + 1) * 100, window.innerHeight - 180); // almost bottom-left corner of the canvas
+    knight.animationSpeed = 0.25;
+    knight.anchor.x = .5
+    knight.anchor.y = .5
+    knight.scale.x = 1
+    knight.vx = 0
+    knight.vy = 0
+    knight.gotoAndStop(1);
 
-        knight.width=300;
-        knight.height=300;
+    knight.width = 300;
+    knight.height = 300;
 
-        // add it to the stage and render!
-        app.stage.addChild(knight);
-        knights.push(knight)
-    }
+    // add it to the stage and render!
+    app.stage.addChild(knight);
+    knights.push(knight)
+  }
 }
 
 function playKnights() {
-    knights.forEach((knight, index) => {
-        knight.play();
-      });
+  knights.forEach((knight, index) => {
+    knight.play();
+  });
 }
 
 function stopKnights() {
-    knights.forEach((knight, index) => {
-        knight.gotoAndStop(1);
-      });
+  knights.forEach((knight, index) => {
+    knight.gotoAndStop(1);
+  });
 }
 
 function moveKnights() {
-    knights.forEach((knight, index) => {
-        knight.x += python.vx;
-        knight.y += python.vy
-      });
+  knights.forEach((knight, index) => {
+    knight.x += python.vx;
+    knight.y += python.vy
+  });
 }
---
+++ src/util.js
@@ -0,0 +1,41 @@
+export function showResume(app) {
+    showPDF(app, 'ryan_coughlin_resume.pdf');
+}
+
+export function showChinese(app) {
+    showPDF(app, 'ryan_coughlin_sample_chinese.pdf');
+}
+
+function showPDF(app, name) {
+    var pdfjsLib = require('pdfjs-dist');
+    var pdfPath = `assets/docs/${name}`;
+    // $("a").attr("href", pdfPath); //set download path
+  
+    var loadingTask = pdfjsLib.getDocument(pdfPath);
+  
+    var loadingTask = pdfjsLib.getDocument(pdfPath);
+    loadingTask.promise.then(function (pdfDocument) {
+      // Request a first page
+      return pdfDocument.getPage(1).then(function (pdfPage) {
+        // Display page on the existing canvas with 100% scale.
+  
+        var canvas = document.getElementById('pdfjs');
+        canvas.style.display = "inherit"
+  
+        app.view.style.display = "none";
+  
+        var viewport = pdfPage.getViewport(3.0);
+  
+        canvas.width = viewport.width;
+        canvas.height = viewport.height;
+        var ctx = canvas.getContext('2d');
+        var renderTask = pdfPage.render({
+          canvasContext: ctx,
+          viewport: viewport
+        });
+        return renderTask.promise;
+      });
+    }).catch(function (reason) {
+      console.error('Error: ' + reason);
+    });
+  }
