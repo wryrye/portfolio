84228c8b0a833682536bcfaae499e747c4e13ea5
Ryan Coughlin <ryan.coughs@gmail.com>
2019-08-22 22:52:12 -0400

    refactor posse pt. 2

--- src/posse.js
+++ src/posse.js
@@ -5,62 +5,63 @@ export default class Posse {
     constructor(app, leader, follower) {
       this.stage = app.stage;
       this.resources = app.loader.resources;
-      this.python = leader;
-      this.members = [];
+      this.leader = leader;
+      this.follower = follower;
+      this.followers = [];
       
-      this.addMembers();
+      this.addFollowers();
       this.faceRight();
       this.play();
     }
 
-    addMembers() {
+    addFollowers() {
       for (let i = 0; i < 3; i++) {
-        let member = new PIXI.AnimatedSprite(this.resources["knight"].spritesheet.animations["knight"]);    
-        member.position.set((i + 1) * window.innerHeight/7 - window.innerHeight, window.innerHeight * 8/10);
-        member.animationSpeed = 0.25;
-        member.anchor.x = .5
-        member.anchor.y = .5
-        member.scale.x = 1
-        member.vx = 0
-        member.vy = 0
-        member.width = window.innerHeight/2.2;
-        member.height = window.innerHeight/2.2;
-        member.gotoAndStop(1);
+        let follower = new PIXI.AnimatedSprite(this.resources[this.follower].spritesheet.animations[this.follower]);    
+        follower.position.set((i + 1) * window.innerHeight/7 - window.innerHeight, window.innerHeight * 8/10);
+        follower.animationSpeed = 0.25;
+        follower.anchor.x = .5
+        follower.anchor.y = .5
+        follower.scale.x = 1
+        follower.vx = 0
+        follower.vy = 0
+        follower.width = window.innerHeight/2.2;
+        follower.height = window.innerHeight/2.2;
+        follower.gotoAndStop(1);
     
-        this.stage.addChild(member);
-        this.members.push(member)
+        this.stage.addChild(follower);
+        this.followers.push(follower)
       }
     }
     
     play() {
-      this.members.forEach((member, index) => {
-        member.play();
+      this.followers.forEach((follower, index) => {
+        follower.play();
       });
     }
     
     stop() {
-      this.members.forEach((member, index) => {
-        member.gotoAndStop(1);
+      this.followers.forEach((follower, index) => {
+        follower.gotoAndStop(1);
       });
     }
     
     move() {
-      this.members.forEach((member, index) => {
-        member.x += this.python.vx;
-        member.y += this.python.vy
+      this.followers.forEach((follower, index) => {
+        follower.x += this.leader.vx;
+        follower.y += this.leader.vy
       });
     }
     
     faceLeft() {
-      this.members.forEach((member, index) => {
-        if (member.scale.x > 1) member.scale.x *= -1;
+      this.followers.forEach((follower, index) => {
+        if (follower.scale.x > 1) follower.scale.x *= -1;
         this.play();
       });
     }
     
     faceRight() {
-      this.members.forEach((member, index) => {
-        if (member.scale.x < 1) member.scale.x *= -1;
+      this.followers.forEach((follower, index) => {
+        if (follower.scale.x < 1) follower.scale.x *= -1;
         this.play();
       });
     }
