7af92063c400270956fd3fb226c9e6848e1df990
Ryan Coughlin <ryan.coughlin@washpost.com>
2022-11-06 19:57:03 -0500

    build

--- docs/assets/img/knight/rename.js
+++ docs/assets/img/knight/rename.js
@@ -1,16 +1,15 @@
-
-const { join } = require('path');
-const { readdirSync, renameSync } = require('fs');
+const { join } = require("path");
+const { readdirSync, renameSync } = require("fs");
 const [dir, search, replace] = process.argv.slice(2);
-const match = RegExp(search, 'g');
+const match = RegExp(search, "g");
 const files = readdirSync(dir);
 
 files
-  .filter(file => file.match(match))
-  .forEach(file => {
+  .filter((file) => file.match(match))
+  .forEach((file) => {
     const filePath = join(dir, file);
 
-    const newFileName = file.substring(0,4) + "_" + file.substring(4);
+    const newFileName = file.substring(0, 4) + "_" + file.substring(4);
     const newFilePath = join(dir, newFileName);
 
     console.log(newFilePath);
